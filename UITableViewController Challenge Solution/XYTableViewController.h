9f8a1e6531af4fbdb8eab7a10bea3c4afff77a02
Xiaoyan Yan <XiaoyanYan@Xiaoyans-MacBook-Air.local>
2014-09-19 21:16:35 -0500

    second commit

---
+++ UITableViewController Challenge Solution/XYTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Xiaoyan Yan on 9/19/14.
+//  Copyright (c) 2014 Xiaoyan Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYTableViewController : UITableViewController
+
+@end
